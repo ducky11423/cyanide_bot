031679b4faa69bd0b65945efc69b38d4b9af3038
Joshua N <joshuanits@gmail.com>
2018-07-04 01:44:10 +0000

    Fixed shit

--- cyanide_bot.py
+++ cyanide_bot.py
@@ -8,6 +8,7 @@ import random
 import datetime
 import time
 import threading
+import sys
 
 data = {}
 data["channels"] = []
@@ -16,7 +17,10 @@ data["apikey"] = ""
 print("Creating discord client")
 h = http.client.HTTPConnection('explosm.net')
 client = discord.Client()
-savefile = "candh.json"
+savefile = "data.json"
+
+def formatComicContent(id):
+    return "Comic ID " + id + ", permlink: http://explosm.net/comics/" + id
 
 def saveData():
     dataString = json.dumps(data, indent = 2)
@@ -26,17 +30,17 @@ def saveData():
 
 async def checkTimer():
     lastChecked = 0
-    time.sleep(1) #Make sure we dont check before client has joined?
+    time.sleep(1000) #Make sure we dont check before client has joined?
     
     if lastChecked != datetime.datetime.now().hour:
         lastChecked = datetime.datetime.now().hour
-        await checkNew()
+        await checkNew(None, None)
     
     t = threading.Timer(60, checkTimer)
     t.start()
         
 
-async def checkNew():
+async def checkNew(manual, channel):
     h.request("GET", "/comics/latest")
     r = h.getresponse()
     if r.status == 200:
@@ -44,18 +48,19 @@ async def checkNew():
         previdloc = d.find(b"/comics/", d.find(b"nav-button_first")) + 8
         previd = int(d[previdloc:d.find(b"/", previdloc)])
         currId = previd + 1
-        print(str(currId) not in data["comics"])
         if str(currId) not in data["comics"]:
             imgstart = d.find(b"src=\"//", d.find(b"main-comic")) + 7
             imgend = d.find(b"\"", imgstart)
             img = str(d[imgstart:imgend], 'utf-8')
             data["comics"][currId] = "http://" + img
             saveData()
-            urllib.request.urlretrieve(data["comics"][currId], "images/" + str(currId) + ".png")
+            urllib.request.urlretrieve(data["comics"][currId], "/home/images/" + str(currId) + ".png")
             
             for a in data["channels"]:
                 channel = client.get_channel(a["channel"])
-                await client.send_file(channel, "images/" + str(currId) + ".png", content="New comic!")
+                await client.send_file(channel, "/home/images/" + str(currId) + ".png", content="New comic!")
+        elif manual:
+            await client.send_message(channel, "No new comic found :(")
     h.close()
 
 @client.event
@@ -67,7 +72,7 @@ async def on_ready():
 
     saveData()
 
-    await checkTimer()
+    #await checkTimer()
 
 @client.event
 async def on_message(message):
@@ -85,7 +90,7 @@ async def on_message(message):
             saveData()
     
     if(message.content == ".check"):
-        await checkNew()
+        await checkNew(True, message.channel)
     
     if(message.content.startswith(".comic")):
         parts = message.content.split(' ')
@@ -93,12 +98,14 @@ async def on_message(message):
             await client.send_message(message.channel, "You need to put an id in!")
         else:
             if(parts[1] == "latest"):
-                await client.send_file(message.channel, "images/" + max(data["comics"], key=int) + ".png", content="Comic ID " + max(data["comics"], key=int))
+                comic = max(data["comics"], key=int)
+                await client.send_file(message.channel, "/home/images/" + comic + ".png", content=formatComicContent(comic))
             elif(parts[1] == "random"):
-                await client.send_file(message.channel, "images/" + random.choice(list(data["comics"].keys())) + ".png")
+                comic = random.choice(list(data["comics"].keys()))
+                await client.send_file(message.channel, "/home/images/" + comic + ".png", content=formatComicContent(comic))
             else:
-                if(os.path.isfile("images/" + parts[1] + ".png")):
-                    await client.send_file(message.channel, "images/" + parts[1] + ".png")
+                if(os.path.isfile("/home/images/" + parts[1] + ".png")):
+                    await client.send_file(message.channel, "/home/images/" + parts[1] + ".png", content=formatComicContent(parts[1]))
                 else:
                     await client.send_message(message.channel, "Sorry, I don't have that comic.")
     
@@ -113,39 +120,41 @@ if(os.path.isfile(savefile)):
     saveData()
     print("Loaded data")
 
+    if("check" in sys.argv):
+        for i in range(0, 4974):
+            currId = i
+            if str(currId) not in data["comics"]:
+                print("Attempting to get comic " + str(i))
+                h.request("GET", "/comics/" + str(i))
+                r = h.getresponse()
+                
+                d = r.read()
+                if b"Could not find comic" not in d:
+                    
+                    imgstart = d.find(b"src=\"//", d.find(b"main-comic")) + 7
+                    imgend = d.find(b"\"", imgstart)
+                    img = str(d[imgstart:imgend], 'utf-8')
+                    data["comics"][currId] = "http://" + img
+
+                    try:
+                        urllib.request.urlretrieve(data["comics"][currId], "/home/images/" + str(currId) + ".png")
+                    except Exception as e:
+                        print(e)
+                        print("Failed to download comic :( (" + data["comics"][currId] + ")")
+
+                    print("Downloading comic " + str(i))
+                    
+                else:
+                    data["comics"][currId] = "not_found"
+                    print("Comic " + str(i) + " not found :/")
+
+                saveData()
+
+
     print("Connecting")
+
+        
+
     client.run(data["apikey"])
 else:
     print("Could not find save data (candh.json). Creating now. Please fill in the apikey and then restart.")
-
-
-
-'''
-for i in range(0, 4926):
-    currId = i
-    if str(currId) not in data:
-        print("Attempting to get comic " + str(i))
-        h.request("GET", "/comics/" + str(i))
-        r = h.getresponse()
-        
-        d = r.read()
-        if b"Could not find comic" not in d:
-            
-            imgstart = d.find(b"src=\"//", d.find(b"main-comic")) + 7
-            imgend = d.find(b"\"", imgstart)
-            img = str(d[imgstart:imgend], 'utf-8')
-            data[currId] = "http://" + img
-
-            try:
-                urllib.request.urlretrieve(data[currId], "images/" + str(currId) + ".png")
-            except:
-                print("Failed to download comic :(")
-
-            print("Downloading comic " + str(i))
-            
-        else:
-            data[currId] = "not_found"
-            print("Comic " + str(i) + " not found :/")
-
-        saveData()
-'''
